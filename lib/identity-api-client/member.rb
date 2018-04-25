0772e8b99ab0026b6bde3a65b5afde8e039bb948
Jacinda Moore <jacinda@controlshiftlabs.com>
2018-04-25 14:39:44 -0400

    add support for passing the load_current_consents parameter to the member details API

--- lib/identity-api-client/member.rb
+++ lib/identity-api-client/member.rb
@@ -1,7 +1,10 @@
 module IdentityApiClient
   class Member < Base
-    def details(guid)
+    def details(guid, load_current_consents: false)
       params = {'guid' => guid, 'api_token' => client.connection.configuration.options[:api_token]}
+      if load_current_consents
+        params['load_current_consents'] = true
+      end
 
       resp = client.post_request('/api/member/details', params)
       resp.body
