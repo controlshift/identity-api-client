a58f1ac16edaadc10b6828172ab710a01a1edd67
Jacinda Moore <jacinda@controlshiftlabs.com>
2018-04-26 12:25:03 -0400

    add support for passing the load_current_consents parameter to the member details API (#1)

--- spec/member_spec.rb
+++ spec/member_spec.rb
@@ -4,23 +4,42 @@ describe IdentityApiClient::Member do
   subject { IdentityApiClient.new(host: 'test.com', api_token: '1234567890abcdef') }
 
   let(:request_path) { '/api/member/details' }
-  let(:request_body) { fixture('request.json').chomp }
 
   describe 'success' do
     let(:status) { 200 }
-    let(:body) { fixture('details.json') }
 
     before(:each) do
-      stub_post(request_path).with(body: request_body).to_return(:body => body, :status => status,
-                                                                 :headers => {:content_type => "application/json; charset=utf-8"})
+      stub_post(request_path).with(body: expected_request).to_return(:body => body, :status => status,
+                                                                     :headers => {:content_type => "application/json; charset=utf-8"})
     end
 
-    it 'should get member details back from the API' do
-      resp = subject.member.details('abcdef1234567890')
+    context 'without load_current_consents' do
+      let(:expected_request) { {'guid' => 'abcdef1234567890', 'api_token' => '1234567890abcdef'}.to_json }
+      let(:body) { fixture('details.json') }
 
-      expect(resp.first_name).to eq('Joe')
-      expect(resp.last_name).to eq('Bloggs')
-      expect(resp.email).to eq('joe@bloggs.com')
+      it 'should get member details back from the API' do
+        resp = subject.member.details('abcdef1234567890')
+
+        expect(resp.first_name).to eq('Joe')
+        expect(resp.last_name).to eq('Bloggs')
+        expect(resp.email).to eq('joe@bloggs.com')
+      end
+    end
+
+    context 'with load_current_consents' do
+      let(:expected_request) { {'guid' => 'abcdef1234567890', 'api_token' => '1234567890abcdef', 'load_current_consents' => true}.to_json }
+      let(:body) { fixture('details_with_consents.json') }
+
+      it 'should get member details with consents back from the API' do
+        resp = subject.member.details('abcdef1234567890', load_current_consents: true)
+
+        expect(resp.first_name).to eq('Joe')
+        expect(resp.last_name).to eq('Bloggs')
+        expect(resp.email).to eq('joe@bloggs.com')
+
+        expect(resp.consents.count).to eq 2
+        expect(resp.consents[0].public_id).to eq 'terms_of_service_1.0'
+      end
     end
   end
 end
