19410b2fe3b5e768bf9d44ec2cf777807bc01661
Rich Shakespeare <richshakespeare@hotmail.co.uk>
2019-04-03 15:20:54 +0100

    implement route

--- lib/identity-api-client/lists.rb
+++ lib/identity-api-client/lists.rb
@@ -2,6 +2,17 @@ require_relative 'list'
 
 module IdentityApiClient
   class Lists < Base
+    def create_from_csv(name, csv_url, callback_url)
+      params = {
+        'api_token' => client.connection.configuration.options[:api_token],
+        'name' => name,
+        'csv_url' => csv_url,
+        'callback_url' => callback_url
+      }
+      resp = client.post_request('/api/lists/create_from_csv', params)
+      return resp.status == 200
+    end
+
     def find_by_id(id)
       resp = client.get_request("/api/lists/#{id}?api_token=#{client.connection.configuration.options[:api_token]}")
       if resp.status == 200
